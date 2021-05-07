113adc5db3ab1013bb395736d43bfde29da4f89a
Daniel Verdú <verdudaniel7@gmail.com>
2021-05-07 08:06:05 +0200

    modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
-console.log('App Works');
+const btnOpenModal = document.querySelector('.btn-modal');
+const btnCloseModal = document.querySelector('.modal-close');
+const modal = document.querySelector('.custom-modal');
+const modalContent = document.querySelector('.modal-content');
+
+console.log(modal)
+
+const addClass = (ele, className) => ele.classList.add(className);
+
+const removeClass = (ele, className) => ele.classList.remove(className);
+
+
+
+const showModal = () => {
+    addClass(modal, 'custom-modal-open');
+    addClass(modalContent, 'show');
+    removeClass(modalContent, 'hide');
+};
+
+const closeModal = () => {
+    removeClass(modal, 'custom-modal-open');
+    addClass(modalContent, 'hide');
+    removeClass(modalContent, 'show');
+}
+
+btnOpenModal.addEventListener('click', () => {
+    showModal();
+});
+
+modal.addEventListener('click', (e) => {
+    if(e.target.getAttribute('data-id') === 'modal' || e.target === btnCloseModal){
+        closeModal();
+    }
+    else{
+        return 
+    }
+    
+})
